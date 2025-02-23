71de37f379169ab6b87347009eb4449a075dc0c4
akshay <akshayellendula@example.com>
2025-02-23 19:25:49 +0530

    improved login view

--- login/login_signup.js
+++ login/login_signup.js
@@ -2,10 +2,17 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
+    document.body.style.backgroundColor="#1a1a1a";
+   const badge = document.getElementsByClassName("badge");
+    badge[0].style.color = "#effe8b";
 });
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+    document.body.style.backgroundColor="#effe8b";
+    const badge1= document.getElementsByClassName("badge");
+    badge1[0].style.color = "#1a1a1a";
 });
