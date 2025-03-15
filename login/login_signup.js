cf2d99a51242fc5c15db28f8e9af1735dbb73a50
akshay <akshayellendula@example.com>
2025-03-16 02:48:48 +0530

    added service and mofied loginpage

--- login/login_signup.js
+++ login/login_signup.js
@@ -1,18 +1,29 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpToggle = document.getElementById('signUpToggle');
+const signInToggle = document.getElementById('signInToggle');
 const container = document.getElementById('container');
+const signInForm = document.getElementById('signInForm');
+const signUpForm = document.getElementById('signUpForm');
 
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-    document.body.style.backgroundColor="#1a1a1a";
-   const badge = document.getElementsByClassName("badge");
-    badge[0].style.color = "#effe8b";
+signUpToggle.addEventListener('click', () => {
+    signUpForm.classList.remove('hidden-form');
+    signUpForm.classList.add('active-form');
+    signInForm.classList.remove('active-form');
+    signInForm.classList.add('hidden-form');
+    signUpToggle.classList.add('active');
+    signInToggle.classList.remove('active');
+    document.body.style.backgroundColor = "#1a1a1a"; 
+    const badge = document.getElementsByClassName("badge");
+    badge[0].style.color = "#effe8b";    
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-    document.body.style.backgroundColor="#effe8b";
-    const badge1= document.getElementsByClassName("badge");
-    badge1[0].style.color = "#1a1a1a";
+signInToggle.addEventListener('click', () => {
+    signInForm.classList.remove('hidden-form');
+    signInForm.classList.add('active-form');
+    signUpForm.classList.remove('active-form');
+    signUpForm.classList.add('hidden-form');
+    signInToggle.classList.add('active');
+    signUpToggle.classList.remove('active');
+    document.body.style.backgroundColor = "#effe8b";
+    const badge = document.getElementsByClassName("badge");
+    badge[0].style.color = "#1a1a1a";
 });
